d008176933670ba6d36e34c3a452a36c53112a50
abelbour <abelbour@users.noreply.github.com>
2019-02-27 21:00:23 -0300

    Create pwabuilder-sw.js

---
+++ pwabuilder-sw.js
@@ -0,0 +1,103 @@
+//This is the service worker with the Cache-first network
+
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+ 'index.html',
+ 'favicon.ico', 
+ 'inicio.html', 
+ 'menu.html', 
+ 'css/PTSans.css', 
+ 'css/bootstrap-theme.min.css', 
+ 'css/bootstrap.min.css', 
+ 'css/cartodb.css', 
+ 'css/jquery-select-step.css', 
+ 'css/jquery.bootstrap-touchspin.min.css', 
+ 'fonts/PTSans-bold.woff2', 
+ 'fonts/PTSans.woff2', 
+ 'fonts/glyphicons-halflings-regular.eot', 
+ 'fonts/glyphicons-halflings-regular.svg', 
+ 'fonts/glyphicons-halflings-regular.ttf', 
+ 'fonts/glyphicons-halflings-regular.woff', 
+ 'fonts/glyphicons-halflings-regular.woff2', 
+ 'js/angular-route.min.js', 
+ 'js/angular.min.js', 
+ 'js/bootstrap.js', 
+ 'js/bootstrap.min.js', 
+ 'js/cartodb.js', 
+ 'js/config.js', 
+ 'js/jquery-select-step.js', 
+ 'js/jquery.bootstrap-touchspin.min.js', 
+ 'js/jquery.min.js', 
+ 'js/json3.min.js', 
+ 'js/jsonQ.min.js', 
+ 'js/script.js', 
+ 'js/viz.json', 
+ 'predicacion/campaña.html', 
+ 'predicacion/grupos.html', 
+ 'predicacion/informar.html', 
+ 'predicacion/publica.html', 
+ 'predicacion/salidas.html', 
+ 'predicacion/telefonica.html', 
+ 'predicacion/territorios.html', 
+ 'reuniones/emision.html', 
+ 'reuniones/programa.html', 
+ 'static/campana.html', 
+ 'static/grupos.html', 
+ 'static/index.html', 
+ 'static/informar.html', 
+ 'static/territorios.html', 
+ 'util/util.html'
+    ];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('[PWA Builder] The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('[PWA Builder] Skip waiting on install');
+    return self.skipWaiting();
+  }));
+});
+
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+  console.log('[PWA Builder] Claiming clients for current page');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('[PWA Builder] The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+function fromServer(request){
+  //this is the fallback if it is not in the cache to go to the server and get it
+  return fetch(request).then(function(response){ return response});
+}
