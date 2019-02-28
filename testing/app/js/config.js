4887948202bcd35d19bcd9c7b1b11e2229c42707
abelbour <abelbour@users.noreply.github.com>
2019-02-28 14:51:29 -0300

    Update config.js

--- testing/app/js/config.js
+++ testing/app/js/config.js
@@ -45,7 +45,7 @@
     window.__env.apiUrl = 'http://dev.your-api.com';
 
     // Base url
-    window.__env.baseUrl = '/';
+    window.__env.baseUrl = 'app/';
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
     window.__env.enableDebug = true;
