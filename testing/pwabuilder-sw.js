593ca492fe90fe9355c87e495824e15312a03386
abelbour <abelbour@users.noreply.github.com>
2019-02-28 21:27:54 -0300

    Update pwabuilder-sw.js

--- testing/pwabuilder-sw.js
+++ testing/pwabuilder-sw.js
@@ -36,7 +36,8 @@ var precacheFilesCors = [
  '//docs.google.com/spreadsheets/d/1BzuB98iqVhP6h2hr6RJELaRFOc9O2A5bj74mvQyapSQ/pubhtml',
  '//docs.google.com/spreadsheets/d/1JuiB_XVz-jO_0Yrzu82pF2d3cbWkDhBwhcp5wF2lOrA/pubhtml?gid=0&single=false&widget=false&headers=false&chrome=false',
  '//fonts.googleapis.com/css?kit=g_LZtaoq_teJt-nj1lQIUoSKLf7XBDVIYWf0YGjmOuE',
- '//ssl.gstatic.com/docs/spreadsheets/publishheader.png'
+ '//ssl.gstatic.com/docs/spreadsheets/publishheader.png',
+ '//ssl.gstatic.com/docs/spreadsheets/waffle_sprite53.png'
     ];
 
 var precacheFilesNoCors = [
