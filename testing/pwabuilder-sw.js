632451c1ce75aea64b0fc63300c9afa60c3a46af
abelbour <abelbour@users.noreply.github.com>
2019-02-28 21:25:18 -0300

    Update pwabuilder-sw.js

--- testing/pwabuilder-sw.js
+++ testing/pwabuilder-sw.js
@@ -35,12 +35,12 @@ var precacheFiles = [
 var precacheFilesCors = [
  '//docs.google.com/spreadsheets/d/1BzuB98iqVhP6h2hr6RJELaRFOc9O2A5bj74mvQyapSQ/pubhtml',
  '//docs.google.com/spreadsheets/d/1JuiB_XVz-jO_0Yrzu82pF2d3cbWkDhBwhcp5wF2lOrA/pubhtml?gid=0&single=false&widget=false&headers=false&chrome=false',
- '//docs.google.com/static/spreadsheets2/client/css/471841686-waffle_k_ltr.css',
  '//fonts.googleapis.com/css?kit=g_LZtaoq_teJt-nj1lQIUoSKLf7XBDVIYWf0YGjmOuE',
  '//ssl.gstatic.com/docs/spreadsheets/publishheader.png'
     ];
 
 var precacheFilesNoCors = [
+  '//docs.google.com/static/spreadsheets2/client/css/471841686-waffle_k_ltr.css'
     ];
 
 //Install stage sets up the cache-array to configure pre-cache content
