305d79528248011bd7c94bd669508492291cd169
Gabriel Macus <gabrielmacus@gmail.com>
2019-05-30 01:00:43 -0300

    Angularjs

--- app/js/script.js
+++ app/js/script.js
@@ -98,6 +98,7 @@ app.controller('cfgController', function ($scope, __env) {
 
 
 app.controller('transmision', ['$scope', '$sce', '__env', function ($scope, $sce, __env) {
+    alert('demo');
           var congregacion = 'Churruarín';
         var hash;
         $scope.transmision = {}
