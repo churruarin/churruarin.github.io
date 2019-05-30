d8e3aeba0a6f5ce5ab3cd5c27ee1c7df21341f31
Gabriel Macus <gabrielmacus@gmail.com>
2019-05-30 01:28:12 -0300

    fixes

--- app/js/script.js
+++ app/js/script.js
@@ -99,7 +99,7 @@ app.controller('cfgController', function ($scope, __env) {
 
 app.controller('transmision', ['$scope', '$sce', '__env', function ($scope, $sce, __env) {
     
-        var congregacion = 'Churruarín';
+        var congregacion = 'Gazzano';//'Churruarín';
         var hash;
         $scope.transmision = {}
     
