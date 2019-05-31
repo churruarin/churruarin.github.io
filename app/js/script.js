c554159fbc0349b1c5af3d3eadaad6b845374b9e
abelbour <abelbour@users.noreply.github.com>
2019-05-31 00:20:28 -0300

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -147,7 +147,7 @@ app.controller('transmision', ['$scope', '$sce', '__env', function ($scope, $sce
            
            
                 var url = decrypt(hash, $scope.transmision.clave);//decrypt(hash, document.getElementById('clave').value);
-                var urlCompleta = "//youtube.com/embed/"+url+"?autoplay=1&modestbranding=1&showinfo=0&rel=0&theme=light&color=white";
+                var urlCompleta = "https://youtube.com/embed/"+url+"?autoplay=1&modestbranding=1&showinfo=0&rel=0&theme=light&color=white";
 
                 if(urlCompleta.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=))([\w\-]{10,12})\b/))
                 {
