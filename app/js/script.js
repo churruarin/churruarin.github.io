19cf9c2d27a3a1065d9ac860e301f1b60a1c4e2d
abelbour <abelbour@users.noreply.github.com>
2019-12-26 11:09:13 -0300

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -124,10 +124,7 @@ app.controller('transmision', ['$scope', '$sce', '__env',  '$http', function ($s
     
     this.config = {
                 sources:[
-              {src: $sce.trustAsResourceUrl("http://abelbour.flashmediacast.com:1935/abelbour/livestream/manifest.mpd"), type: "application/dash+xml"},
-
-                {src: $sce.trustAsResourceUrl("http://abelbour.flashmediacast.com:1935/abelbour/livestream/playlist.m3u8"), type: "application/x-mpegURL"}
-                  
+              {src: $sce.trustAsResourceUrl("http://abelbour.flashmediacast.com:1935/abelbour/livestream/manifest.mpd"), type: "application/dash+xml"}                  
                 ],
                 theme: {
                     url: "//unpkg.com/videogular@2.1.2/dist/themes/default/videogular.css"
