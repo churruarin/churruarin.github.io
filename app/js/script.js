6134171f5bad84e3018fce9a7b733175e52fd9f0
abelbour <abelbour@users.noreply.github.com>
2019-12-26 10:37:35 -0300

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -124,7 +124,7 @@ app.controller('transmision', ['$scope', '$sce', '__env',  '$http', function ($s
     
     this.config = {
                 sources: [
-                    {src: $sce.trustAsResourceUrl("http://ad.mysisli.com/live/abelbour/index.m3u8"), type: "application/x-mpegURL"}
+                    {src: $sce.trustAsResourceUrl("http://abelbour.flashmediacast.com:1935/abelbour/livestream/playlist.m3u8"), type: "application/x-mpegURL"}
                   
                 ],
                 theme: {
