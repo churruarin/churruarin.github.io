4393234b53a11ed01657e5576876eb7c67310f16
abelbour <abelbour@users.noreply.github.com>
2021-07-24 23:48:35 -0300

    Update tel.js

--- tel/test/tel.js
+++ tel/test/tel.js
@@ -66,9 +66,9 @@ async function contactos(tipo,nombre,refresh) {
   if (refresh === true || typeof jsonContactos === 'undefined')
   await $.getJSON(urlContactos).done(function (jsonurl) {
     jsonContactos = jsonata(
-      '$.values.({"Telefono":$[0], "Direccion":$[1], "Localidad":$[2], "Fecha":$[3], "Respuesta":$[4], "Publicador":$[5], "Turno":$[6], "Observaciones":$[7], "Responsable":$[8],' + 
+      '$.values.({"Telefono":$[0], "Direccion":($[2]="Campaña celulares 2021"? ($eval($[1])) :$[1]), "Localidad":$[2], "Fecha":$[3], "Respuesta":$[4], "Publicador":$[5], "Turno":$[6], "Observaciones":$[7], "Responsable":$[8],'+
       '"Timestamp":$toMillis($[9],"[Y0001]-[M01]-[D01]T[H01]:[m01]:[s01]"),"TimestampIso":$fromMillis($toMillis($[9],"[Y0001]-[M01]-[D01]T[H01]:[m01]:[s01]"), "[D01]/[M01]/[Y0001] [H01]:[m01]"),'+
-      '"DireccionP":$[1] & ", " & $[2], "FechaP": ($[3] & ($boolean($[6]) ?(" por la "& $[6]) : ""))})'
+      '"DireccionP":($[2]="Campaña celulares 2021"? () : $[1] & ", " & $[2]), "FechaP": ($[3] & ($boolean($[6]) ?(" por la "& $[6]) : ""))})'
       ).evaluate(jsonurl);
   });
   var contactos
