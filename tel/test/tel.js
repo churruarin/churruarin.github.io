0b094201a2fc75c181c37351e08c7078882edaff
abelbour <abelbour@users.noreply.github.com>
2021-08-04 20:39:19 -0300

    Update tel.js

--- tel/test/tel.js
+++ tel/test/tel.js
@@ -868,7 +868,7 @@ $(document).ready(function () {
     };
     var history = (typeof selectedRecord.responsable.history === "undefined")?[]:selectedRecord.responsable.history;
     selectedRecord.responsable.history=jsonata('[$map($append(["'+selectedRecord.publicador.publicador.Nombre+'"],$[$!="'+selectedRecord.publicador.publicador.Nombre+'"]), function($v, $i) {$i<5?$v})]').evaluate(history);
-    Cookies.set("history", JSON.stringify(history));
+    Cookies.set("history", JSON.stringify(selectedRecord.responsable.history));
     if (await submit(dataJson)) {
       // var link = await waLink(selectedPub["Nombre"],contacto);
       window.open(await waLink(await generarMensaje(selectedRecord.publicador.dataReservas.tipoReserva)));
