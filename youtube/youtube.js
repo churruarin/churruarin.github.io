b60ee2c296b2cf1aedb5b20e2b58fb1e9f15970f
abelbour <abelbour@users.noreply.github.com>
2022-05-21 19:09:00 -0300

    Update youtube.js

--- youtube/youtube.js
+++ youtube/youtube.js
@@ -1,40 +1,33 @@
 var LinkYT;
-var online;
+var online = false;
 
 const settings = {
 
 };
 const urls = {
-  urllinkYT:
-    "https://sheets.googleapis.com/v4/spreadsheets/1A2JFdnLfTCNeWCui3_IZ69X201WRHVHr72rwh6n-1oA/values/LinkYT?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
   urlonline:
     "https://sheets.googleapis.com/v4/spreadsheets/1A2JFdnLfTCNeWCui3_IZ69X201WRHVHr72rwh6n-1oA/values/UpdatedToday?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc"
 };
 
 async function isOnline() {
     await $.getJSON(urls.urlonline).done(function (jsonurl) {
-      online = jsonata('$.values.($)').evaluate(
+      var x = jsonata('{"online":$.values[0][0],"link":$.values[1][0]}').evaluate(
         jsonurl
       );
     });
-    link()
-  return online;
+    
+    online = x.online
+    LinkYT = x.link
+  return x;
 }
 
-function link() {
-  $.getJSON(urls.urllinkYT).done(function (jsonurl) {
-    linkYT = jsonata('$.values.($)').evaluate(
-      jsonurl
-    );
-  });
-return linkYT;
-}
 
 async function check() {
-  if(isOnline() === false) {
+  var y = isOnline()
+  if(y.online === false) {
     window.setTimeout(checkFlag, 30000); /* this checks the flag every 100 milliseconds*/
  } else {
-location.href = link();
+location.href = x.link;
  }
 }
 
