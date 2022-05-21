da639faead8ca2d12e11afcfc4ba2b5fe83b068b
abelbour <abelbour@users.noreply.github.com>
2022-05-21 18:43:22 -0300

    Update youtube.js

--- youtube/youtube.js
+++ youtube/youtube.js
@@ -29,4 +29,22 @@ async function link() {
 return linkYT;
 }
 
+async function check() {
+  if(isOnline() === false) {
+    window.setTimeout(checkFlag, 30000); /* this checks the flag every 100 milliseconds*/
+ } else {
+  location.href = link();
+ }
+}
+
+
+$(document).ready(function () {
+  
+
+check()
+
+
+
+});
+
 
