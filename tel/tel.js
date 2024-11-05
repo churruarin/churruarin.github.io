d0ef3276de642cc9a3a2e3c1e970841b95044dac
abelbour <abelbour@users.noreply.github.com>
2024-11-04 23:13:08 -0300

    Update tel.js

--- tel/tel.js
+++ tel/tel.js
@@ -34,13 +34,13 @@ const settings = {
 };
 const urls = {
   revisitas:
-    "https://sheets.googleapis.com/v4/spreadsheets/1BZcHGZONa-M-BVf_CoYuPVStQZEBwS_EzhRirIU3gIg/values/revisitas?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
+    "https://sheets.googleapis.com/v4/spreadsheets/1peVlTnWI4I1abW35dnny_el1k_JU3Pud1RXEchqfB9k/values/revisitas?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
   contactos:
-    "https://sheets.googleapis.com/v4/spreadsheets/1BZcHGZONa-M-BVf_CoYuPVStQZEBwS_EzhRirIU3gIg/values/telefonos2?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
+    "https://sheets.googleapis.com/v4/spreadsheets/1peVlTnWI4I1abW35dnny_el1k_JU3Pud1RXEchqfB9k/values/telefonos2?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
   publicadores:
-    "https://sheets.googleapis.com/v4/spreadsheets/1BZcHGZONa-M-BVf_CoYuPVStQZEBwS_EzhRirIU3gIg/values/pubs?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
+    "https://sheets.googleapis.com/v4/spreadsheets/1peVlTnWI4I1abW35dnny_el1k_JU3Pud1RXEchqfB9k/values/pubs?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
   responsables:
-    "https://sheets.googleapis.com/v4/spreadsheets/1BZcHGZONa-M-BVf_CoYuPVStQZEBwS_EzhRirIU3gIg/values/responsables?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
+    "https://sheets.googleapis.com/v4/spreadsheets/1peVlTnWI4I1abW35dnny_el1k_JU3Pud1RXEchqfB9k/values/responsables?alt=json&key=AIzaSyCz4sutc6Z6Hh5FtBTB53I8-ljkj6XWpPc",
   script:
     "https://script.google.com/macros/s/AKfycbzivt4eVHnlJKOwMIHFq6n200v8eMOkx8qNJOgFf08R-ncjqa_r/exec",
 };
